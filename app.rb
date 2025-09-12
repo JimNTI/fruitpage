50b3f5d05023b860dfd6bd5715570283f174a546
JimPalo <jim.palo@elev.ga.ntig.se>
2025-09-12 09:59:01 +0200

    first

--- app.rb
+++ app.rb
@@ -1,1 +1,25 @@
 require 'sinatra'
+require 'slim'
+require 'sinatra/reloader'
+
+get('/') do
+  slim(:start)
+
+end
+
+get('/bye') do
+  slim(:bye)
+
+end
+
+get ('/name') do
+  @name = "Jim"
+  slim(:name)
+
+end
+
+get('/fruits') do
+ @fruits = ["Äpple",
+"Banan","Apelsin"]
+ slim(:fruits)
+end
